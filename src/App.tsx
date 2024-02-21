f64104ab2dc7b77c5d69ae76be8e09100d3eba5d
parkjineon <truewords0113@naver.com>
2024-02-21 16:33:52 +0900

    Feat: Set Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, RouteObject, Routes } from 'react-router-dom';
+import { mainRouter } from './router/main-router';
+
+
+function renderRoutes(routesObj:RouteObject[]){
+  return routesObj.map((route)=>{
+    if(route.children){
+      return(
+        <Route key={route.path} path={route.path} index={route.index} element={route.element}>
+          {route.children ? renderRoutes(route.children):null}
+        </Route>
+      )
+    }
+
+    return <Route key={route.path} path={route.path} index={route.index} element={route.element} />;
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {renderRoutes(mainRouter)}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/nonLogin/nonLoginMainPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NonLoginMainPage() {
+  return (
+    <div>mainPage</div>
+  )
+}
