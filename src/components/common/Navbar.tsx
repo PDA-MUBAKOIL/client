2139b28c93003b5122fa76d8c424794490da633e
parkjineon <truewords0113@naver.com>
2024-02-23 10:27:20 +0900

    Feat: Create Common Component SearchBar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Logo from "../../assets/img/Nav/logo.svg";
+import styled from 'styled-components'
 
-
-
+const NavBarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px 15px;
+  background-color: #EBDCDC;
+  width: 100%;
+  height: 62px;
+`
+const NavLogo = styled.img`
+  width: 45px;
+  height: 45px;
+`
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      <img src ={Logo} className='navLogo' alt="navLogo"></img>
-    </div>
+    <NavBarContainer>
+      <NavLogo src={Logo} alt="navLogo"></NavLogo>
+    </NavBarContainer>
   )
 }
