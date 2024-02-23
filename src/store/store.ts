3fa316cc699e2b645724c9a1f60fed30c168e756
parkjineon <truewords0113@naver.com>
2024-02-23 17:46:49 +0900

    Feat: Export Redux ReturnType

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import {configureStore} from '@reduxjs/toolkit';
 
+
+
 export const rooterReducer = combineReducers({
 
 });
@@ -8,3 +10,6 @@ export const rooterReducer = combineReducers({
 export const store = configureStore({
     reducer: rooterReducer
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
