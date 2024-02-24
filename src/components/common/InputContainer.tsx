7e13c40f8121fc936888295a2f3d6c7336f7b411
yjp8842 <yjp20183029@gmail.com>
2024-02-25 00:02:43 +0900

    feat: 공통 컴포넌트 입력창 구현

---
+++ src/components/common/InputContainer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+type InputProp = {
+  placeholder: string;
+};
+
+const InputDiv = styled.input`
+  width: 273px;
+  padding: 14px 20px;
+  background-color: #fff;
+  color: #717171;
+  box-shadow: 3px 3px 3px 0 rgb(193, 120, 120, 50%);
+  border-radius: 10px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default function InputContainer({ placeholder }: InputProp) {
+  return <InputDiv placeholder={placeholder} />;
+}
