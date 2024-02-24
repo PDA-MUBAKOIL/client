7b0e947a5ab3158595a9cd7a0416ba3d21c41078
yjp8842 <yjp20183029@gmail.com>
2024-02-25 00:04:56 +0900

    feat: 비로그인시 이메일 인증과 회원 정보 입력하는 컴포넌트 구현

--- src/routes/nonLogin/nonLoginLogIn.tsx
+++ src/routes/nonLogin/nonLoginLogIn.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import CommonButton from "../../components/common/CommonButton";
 
 export default function NonLoginLogIn() {
-  return (
-    <CommonButton
-      text="이메일 인증"
-      onClick={() => console.log("이메일 인증")}
-    />
-  );
+  return <></>;
 }
