347ec210ba57424c140fcb5459a846c85e570a86
yjp8842 <yjp20183029@gmail.com>
2024-02-24 16:58:56 +0900

    design: 배너 디자인 수정

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import { Container, Text } from '@mantine/core';
-import styled from 'styled-components';
+import React from "react";
+import { Text } from "@mantine/core";
+import styled from "styled-components";
 
-const BannerContainer = styled(Container)`
-  background-color: #EBDCDC;
+const BannerContainer = styled.div`
+  background-color: #ebdcdc;
+  width: 100%;
   height: 260px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+  margin-top: 10px;
+`;
 
 const BannerFont = styled(Text)`
   font-size: 17px;
-`
+`;
 
 const LoginButton = styled.div`
-  background-color: #C17878;
+  background-color: #c17878;
   padding: 10px 35px;
   max-width: 115px;
   border-radius: 99px;
   color: #fff;
   margin-top: 15px;
-`
+`;
 
 export default function Banner() {
   return (
     <BannerContainer>
       <BannerFont>로그인을 하고</BannerFont>
       <BannerFont>나만의 컬렉션을 만들어보세요!</BannerFont>
-      <LoginButton onClick={() => console.log('로그인 페이지로 이동')}>로그인</LoginButton>
+      <LoginButton onClick={() => console.log("로그인 페이지로 이동")}>
+        로그인
+      </LoginButton>
     </BannerContainer>
-  )
+  );
 }
