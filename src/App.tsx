5aca57015acf9276080e341b5b26a4f6dae73338
yjp8842 <yjp20183029@gmail.com>
2024-02-25 00:37:20 +0900

    feat: redux-persist 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
-import React from 'react';
-import { BrowserRouter, Route, RouteObject, Routes } from 'react-router-dom';
-import { mainRouter } from './router/main-router';
-import { Provider } from 'react-redux';
-import { store } from './store/store'
-import '@mantine/core/styles.css';
+import React from "react";
+import { BrowserRouter, Route, RouteObject, Routes } from "react-router-dom";
+import { mainRouter } from "./router/main-router";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store";
+import "@mantine/core/styles.css";
+import { PersistGate } from "redux-persist/integration/react";
 
-
-function renderRoutes(routesObj:RouteObject[]){
-  return routesObj.map((route)=>{
-    if(route.children){
-      return(
-        <Route key={route.path} path={route.path} index={route.index} element={route.element}>
-          {route.children ? renderRoutes(route.children):null}
+function renderRoutes(routesObj: RouteObject[]) {
+  return routesObj.map((route) => {
+    if (route.children) {
+      return (
+        <Route
+          key={route.path}
+          path={route.path}
+          index={route.index}
+          element={route.element}
+        >
+          {route.children ? renderRoutes(route.children) : null}
         </Route>
-      )
+      );
     }
 
-    return <Route key={route.path} path={route.path} index={route.index} element={route.element} />;
-  })
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        index={route.index}
+        element={route.element}
+      />
+    );
+  });
 }
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          {renderRoutes(mainRouter)}
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>s{renderRoutes(mainRouter)}</Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,15 +5,50 @@ import cardListReducer from "./reducers/cardList";
 import userReducer from "./reducers/user";
 import emailReducer from "./reducers/Auth/email";
 
-export const rooterReducer = combineReducers({
-  cardList: cardListReducer,
-  user: userReducer,
-  email: emailReducer,
-});
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import logger from "redux-logger";
+
+const rootPersistConfig = {
+  key: "root",
+  storage: storage,
+  whiteList: [],
+};
+
+const myMiddlewares = [logger];
+
+export const rootReducer = persistReducer(
+  rootPersistConfig,
+  combineReducers({
+    cardList: cardListReducer,
+    user: userReducer,
+    email: emailReducer,
+  })
+);
 
 export const store = configureStore({
-  reducer: rooterReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(myMiddlewares);
+    return middlewares;
+  },
 });
 
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
+export type PersistState = ReturnType<typeof persistor.getState>;
 export type AppDispatch = typeof store.dispatch;
