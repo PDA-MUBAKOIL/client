880dfcef86297e6c87b0a2c5f584fa8d28bf4235
yjp8842 <yjp20183029@gmail.com>
2024-02-25 00:03:09 +0900

    design: 버튼 컴포넌트 사이즈 조정

--- src/components/common/CommonButton.tsx
+++ src/components/common/CommonButton.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Button, rem } from '@mantine/core';
+import React from "react";
+import { Button, rem } from "@mantine/core";
 
 type ButtonProp = {
-  text: String,
-  onClick: () => void,
-}
+  text: String;
+  onClick: () => void;
+};
 
 export default function CommonButton({ text, onClick }: ButtonProp) {
   return (
@@ -13,15 +13,16 @@ export default function CommonButton({ text, onClick }: ButtonProp) {
       size="md"
       styles={{
         root: {
-          paddingLeft: rem('107px'), 
-          paddingRight: rem('107px'),
-          height: rem('47px'),
-          backgroundColor: '#C17878' 
+          paddingLeft: rem("107px"),
+          paddingRight: rem("107px"),
+          height: rem("47px"),
+          backgroundColor: "#C17878",
+          maxWidth: "fit-content",
         },
       }}
       onClick={onClick}
     >
       {text}
     </Button>
-  )
+  );
 }
