10a32d7ce973233195e6dae6cfe394e8454c82b5
parkjineon <truewords0113@naver.com>
2024-02-25 20:31:04 +0900

    Fix: Type TSearchResult

--- src/lib/api/drinks.tsx
+++ src/lib/api/drinks.tsx
@@ -25,15 +25,9 @@ export async function listUp() {
         material: "머루, 포도, 백설탕, 효모, 메타중아황산칼륨, 아황산함유",
         __v: 0,
         id: "65d8413288669a99299535ed",
-<<<<<<< HEAD
         percent:"14.5%"
     } as TSearchResult
     })}
-=======
-      } as TSearchResult;
-    }),
-  };
->>>>>>> 77dbb53373e9cecd08a5deb1b9c9efd325c30cad
 }
 
 export async function getDrinkDetail(drinkId: string) {
