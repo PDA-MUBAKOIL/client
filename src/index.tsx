aa78d574e8febd788c1bc05f22676dc24ec78e36
parkjineon <truewords0113@naver.com>
2024-02-28 13:56:03 +0900

    Feat: Set Cordova Environment

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,24 @@ import './index.css';
 import App from './App';
 import { MantineProvider } from '@mantine/core';
 
+const startApp = () =>{
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </React.StrictMode>
+  );
+  
+}
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>
-);
 
 
+if( (window as {cordova: any} & typeof window).cordova ){
+  document.addEventListener('deviceready',startApp,false);
+}else{
+  startApp();
+}
